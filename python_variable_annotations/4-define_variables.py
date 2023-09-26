6fa89d968212a6e407d078dec313935211e56645
Youssef_Saad <6270@holbertonstudents.com>
2023-09-26 15:24:27 +0100

    Define variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""a type-annotated function that define and annotate
+   variables with a specified values"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
