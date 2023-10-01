f53249759b8db8ea32bb933d0ca367920522e5d3
Youssef_Saad <6270@holbertonstudents.com>
2023-10-01 23:35:44 +0100

    Tasks, Fix: fixing the variables and return value

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+
 """Function that creates an asyncio task"""
 
 import asyncio
-from 0-basic_async_syntax import wait_random
+import random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """Function that returns an asyncio task"""
