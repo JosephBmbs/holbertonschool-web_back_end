3f304398af062cfdc39b9ab550605c9a655c3fda
Youssef_Saad <6270@holbertonstudents.com>
2023-10-10 18:07:38 +0100

    For...of Loops,stuck with Passes lint issues

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = [];
+  const NewArra = [];
   for (const idx of array) {
-  newArray.push(appendString + idx);
+    NewArra.push(appendString + idx);
   }
 
-  return newArray;
+  return NewArra;
 }
